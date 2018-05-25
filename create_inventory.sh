457029642b3a5094a0456a513ab0785144162a1b
Yurii Rudenko <yurii.rudenko@masterofcode.com>
2018-05-25 23:27:06 +0300

    fix

--- create_inventory.sh
+++ create_inventory.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-for i in {1..100}
+for i in {1..5}
 do
   id=$(echo $(curl -X POST http://localhost:9292/api/v1/categories \
     -H 'Cache-Control: no-cache' \
     -H 'content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' \
     -F "category[name]=Category$i") | cut -d':' -f 2 | cut -d',' -f 1)
 
-  for j in {1..100}
+  for j in {1..50}
   do
     price=$(( $RANDOM % 10000 ));
     curl -X POST http://localhost:9292/api/v1/products \
@@ -16,5 +16,6 @@ do
       -F "product[name]=Product$j" \
       -F "product[category_id]=$id" \
       -F "product[price]=$price"
+      echo "\n";
   done
 done
